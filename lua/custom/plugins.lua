d2b3c1f852948dbad73fc26d6a2bd5d76b519e63
AbdulAzizRohan <dev.abdulazizrohan@gmail.com>
2024-11-05 11:33:16 +0600

    added refact.ai code compilation

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -142,6 +142,16 @@ local plugins = {
       })
     end,
   },
+  {
+    "smallcloudai/refact-neovim",
+    lazy = false,
+    config = function()
+     require("refact-neovim").setup({
+        address_url = "Refact",
+        api_key = "eBoG4p1JJxIKMS0trdqDlALQ",
+      })
+    end
+  },
   -- {
   --   "mfussenegger/nvim-lint",
   --   event = "VeryLazy",
