3c3ebde23cb4c62d89fe1ee63a0931618242af70
AbdulAzizRohan <dev.abdulazizrohan@gmail.com>
2024-11-06 20:51:41 +0600

    added VimTex for writing latex using neovim

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -142,6 +142,15 @@ local plugins = {
       })
     end,
   },
+  {
+    "lervag/vimtex",
+    lazy = false,     -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      -- vim.g.vimtex_view_method = "zathura"
+    end
+  },
   -- {
   --   "smallcloudai/refact-neovim",
   --   lazy = false,
