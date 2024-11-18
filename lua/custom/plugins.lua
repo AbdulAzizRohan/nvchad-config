f0d7030af43a57ae878b576e842974d51ca9e13e
AbdulAzizRohan <dev.abdulazizrohan@gmail.com>
2024-11-19 00:18:54 +0600

    added silicon for code screenshot

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -170,6 +170,29 @@ local plugins = {
       }
     end
   },
+  {
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    init = function ()
+      local wk = require("which-key")
+      wk.register({
+        ["<leader>sc"] = { ":Silicon<CR>", "Snapshot Code" },
+      }, {mode = "v"})
+    end,
+    config = function ()
+      require("silicon").setup({
+        font = "CaskaydiaMono Nerd Font=34; Noto Color Emoji=34",
+        theme = "Coldark-Dark",
+        background = "#94e2d5",
+        window_title = function ()
+          return vim.fn.fnamemodify(
+            vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t"
+          )
+        end
+      })
+    end
+  },
   -- {
   --   "smallcloudai/refact-neovim",
   --   lazy = false,
